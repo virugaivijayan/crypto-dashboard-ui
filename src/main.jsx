5a1bc4913205f78eee0402d6161722f03352a61a
virugaivijayan <virugaivijayan@gmail.com>
2025-05-09 18:42:02 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import CryptoSignalDashboard from './pages/CryptoSignalDashboard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CryptoSignalDashboard />
+  </React.StrictMode>
+)
 
